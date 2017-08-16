9c25eb16d8ab47b318cb955f083aecc38c30e4b5
fxz328 <ww.f593@qq.com>
2017-08-16 15:59:05 +0800

    test远程分支

---
+++ web-controller/src/main/java/com/jk/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.jk.controller;
+
+/**
+ * PROJECT_NAME:maven-web-parent
+ * name:TestController
+ * User:fan
+ * Time:2017-08-16 15:58
+ */
+public class TestController {
+    public static void main(String[] args) {
+        System.out.print("hehe");
+    }
+}
