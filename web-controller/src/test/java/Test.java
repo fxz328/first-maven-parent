a1fe8fc7b3fca9c843fd742415b2f273e3707a0b
fxz328 <ww.f593@qq.com>
2017-08-16 15:35:20 +0800

    test远程分支

---
+++ web-controller/src/test/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * PROJECT_NAME:maven-web-parent
+ * name:Test
+ * User:fan
+ * Time:2017-08-16 15:32
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
